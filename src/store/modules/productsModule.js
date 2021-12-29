632dcf2dcad7e22a41e76ac6396f644ce76e669a
Zakratus <n.sakevych@gmail.com>
2021-12-29 17:06:51 +0200

    Changed Promise-.then with async func-Await

--- src/store/modules/productsModule.js
+++ src/store/modules/productsModule.js
@@ -2,31 +2,40 @@ import axios from 'axios';
 
 export const productsModule = {
   state: () => ({
-    products: []
+    products: [],
+    isProductsLoading: false,
   }),
   getters: {
     PRODUCTS(state) {
       return state.products;
+    },
+    LOADING(state) {
+      return state.isProductsLoading;
     }
   },
   actions: {
-    GET_PRODUCTS_FROM_API({ commit }) {
-      return axios('http://localhost:3000/products', {
-        method: "GET"
-      })
-        .then(products => {
-          commit('SET_PRODUCTS', products.data);
-          return products;
-        })
-        .catch((err) => {
-          console.log(err)
-          return err
-        })
+    async GET_PRODUCTS_FROM_API({ commit }) {
+      try {
+        commit('SET_LOADING', true);
+        const response = await axios('http://localhost:3000/products', {
+          method: "GET"
+        });
+        commit('SET_PRODUCTS', response.data);
+        return products;
+      } catch (err) {
+        console.log(err)
+        return err
+      } finally {
+        commit('SET_LOADING', false);
+      }
     }
   },
   mutations: {
     SET_PRODUCTS: (state, products) => {
       state.products = products;
+    },
+    SET_LOADING: (state, bool) => {
+      state.isProductsLoading = bool;
     }
   }
 }
