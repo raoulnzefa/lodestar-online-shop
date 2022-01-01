214b3dd23c3a133c169ed0b10e0e2cbb10dada46
Zakratus <n.sakevych@gmail.com>
2022-01-01 19:40:31 +0200

    Cart designe changes

--- src/components/header/LodeHeaderCart.vue
+++ src/components/header/LodeHeaderCart.vue
@@ -1,9 +1,19 @@
 <template>
-  <div
-    class="lode-header__cart"
-    @click.self="showCartList = !showCartList"
-  >
-    Cart {{CART.length ? CART.length : ''}}
+  <div class="lode-header__cart">
+    <img
+      :src="require('../../assets/shopping-cart.png')"
+      alt=""
+      class="lode-header__cart-image"
+      @click.self="showCartList = !showCartList"
+    >
+    <transition name="fade">
+      <div
+        v-show="CART.length"
+        class="lode-header__cart-quantity"
+      >
+        <p>{{CART.length ? CART.length : ''}}</p>
+      </div>
+    </transition>
     <ul
       class="lode-header__cart-list"
       :class="{show: showCartList}"
@@ -94,9 +104,6 @@ export default {
     deleteFromCart(index) {
       this.DELETE_FROM_CART(index);
     },
-    getItemsQuantity(cart) {
-      // cart.quantity.reduce((acc, curr) => (acc += curr));
-    },
   },
 };
 </script>
@@ -109,7 +116,29 @@ export default {
   width: 12.5%;
   padding: 1.5rem 0;
 
-  cursor: pointer;
+  &-image {
+    width: 3.2rem;
+    cursor: pointer;
+  }
+
+  &-quantity {
+    position: absolute;
+    top: 0;
+    right: 0.5rem;
+    z-index: 1;
+
+    width: 1.5rem;
+    height: 1.5rem;
+
+    background-color: $white;
+    border-radius: 50%;
+
+    p {
+      font-size: 1.2rem;
+      line-height: 1.4;
+      color: $black;
+    }
+  }
 
   &-list {
     display: none;
