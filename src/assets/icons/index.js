6c6b07a3383438b7d89f183b6cb7bdf9732320ed
Никита Сакевич <nikita.sakevych@MacBook-Air-Nikita.local>
2022-05-16 16:51:35 +0300

    Added some assets: icons, images, svgs

---
+++ src/assets/icons/index.js
@@ -0,0 +1,8 @@
+export { default as PhoneSvgSrc } from "./phone.svg"
+export { default as MailSvgSrc } from "./mail.svg"
+export { default as MapSvgSrc } from "./map-point.svg"
+export { default as ProfilSvgSrc } from "./profile.svg"
+export { default as HeartSvgSrc } from './heart.svg'
+export { default as HeartFillSvgSrc } from './heart-fill.svg'
+export { default as HeartWhiteSvgSrc } from './heart-white.svg'
+
