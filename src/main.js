e9b4f1698d05b68009a4d1f524f7c1734fbcf5f6
Nikita Sakevych <n.sakevych@gmail.com>
2022-05-20 19:09:01 +0300

    Create local enviroment variables

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import store from '@/store';
 import router from '@/router/router';
 import components from '@/components/UI'
 import '@/assets/styles/variables.scss';
+import env from 'dotenv';
 
 const app = createApp(App);
+env.config();
 
 components.forEach(component => {
   app.component(component.name, component);
