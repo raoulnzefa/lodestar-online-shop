b8d6dad60f0e8c12e77625224a2046eeee9bd3a0
Nikita Sakevych <n.sakevych@gmail.com>
2022-05-31 14:47:44 +0300

    Added ButtonWishlist component

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -2,10 +2,12 @@ import LodeButton from '@/components/UI/LodeButton';
 import LodeInput from '@/components/UI/LodeInput';
 import LodeCheckbox from '@/components/UI/LodeCheckbox';
 import LodeModal from "@/components/UI/LodeModal";
+import LodeButtonWishlist from '@/components/UI/LodeButtonWishlist';
 
 export default [
   LodeButton,
   LodeInput,
   LodeCheckbox,
-  LodeModal
+  LodeModal,
+  LodeButtonWishlist
 ]
